7e856b25ea9b90053d493eb3131c0e1a742fe472
moodytongytong <moodytongytong@protonmail.com>
2023-06-10 17:57:48 +0100

    remove rust executable

--- projects/06/assembler/target/debug/deps/assembler-5d694ff5ebd69e15.d
+++
@@ -1,9 +0,0 @@
-/home/moodytongytong/Dropbox/personal/self_study_resources/CS/github/moodytongytong/nand2tetris/projects/06/assembler/target/debug/deps/assembler-5d694ff5ebd69e15.rmeta: src/lib.rs src/code.rs src/parser.rs
-
-/home/moodytongytong/Dropbox/personal/self_study_resources/CS/github/moodytongytong/nand2tetris/projects/06/assembler/target/debug/deps/libassembler-5d694ff5ebd69e15.rlib: src/lib.rs src/code.rs src/parser.rs
-
-/home/moodytongytong/Dropbox/personal/self_study_resources/CS/github/moodytongytong/nand2tetris/projects/06/assembler/target/debug/deps/assembler-5d694ff5ebd69e15.d: src/lib.rs src/code.rs src/parser.rs
-
-src/lib.rs:
-src/code.rs:
-src/parser.rs:
--- projects/06/assembler/target/debug/deps/assembler-8ae4a6006a2c3184.d
+++
@@ -1,7 +0,0 @@
-/home/moodytongytong/Dropbox/personal/self_study_resources/CS/github/moodytongytong/nand2tetris/projects/06/assembler/target/debug/deps/assembler-8ae4a6006a2c3184.rmeta: src/lib.rs src/code.rs src/parser.rs
-
-/home/moodytongytong/Dropbox/personal/self_study_resources/CS/github/moodytongytong/nand2tetris/projects/06/assembler/target/debug/deps/assembler-8ae4a6006a2c3184.d: src/lib.rs src/code.rs src/parser.rs
-
-src/lib.rs:
-src/code.rs:
-src/parser.rs:
